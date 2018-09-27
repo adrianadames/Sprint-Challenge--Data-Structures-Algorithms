bfce764a5e5dc75a79e2905e5aefcd6c38150165
Adrian Adames <adrianadames@gmail.com>
2018-09-27 19:54:38 -0400

    Binary search tree traveral still in progress.

--- data_structures/ex_1/binary_search_tree.py
+++ data_structures/ex_1/binary_search_tree.py
@@ -15,9 +15,26 @@ class BinarySearchTree:
     self.left = None
     self.right = None
 
-  def depth_first_for_each(self, cb):
-    pass
-  
+  # def depth_first_for_each(self, cb):
+  def depth_first_for_each(self):
+    dfsContainer = []
+    stack = Stack()
+    current = BinarySearchTree(self.value)
+    finished = False
+
+    while finished is False:
+      if current is not None:
+        stack.push(current.value)
+        current = current.left
+      else:
+        if len(stack.items)>0:
+          current = BinarySearchTree(stack.pop())
+          dfsContainer.append(current.value)
+          current = current.right
+        else:
+          finished = True
+    print(dfsContainer)
+
 
   def breadth_first_for_each(self, cb):
     pass
@@ -59,19 +76,30 @@ class BinarySearchTree:
 
 
 
+# stack1 = Stack()
+# stack1.push(4)
+# stack1.push(5)
+# stack1.push(6)
+# print(stack1.items)
+# print(stack1.pop())
+# print(stack1.items)
 
 
 
-
-# bst1 = BinarySearchTree(8)
-# bst1.insert(8)
-# bst1.insert(3)
-# bst1.insert(1)
+bst1 = BinarySearchTree(8)
+bst1.insert(3)
+bst1.insert(1)
 # bst1.insert(6)
 # bst1.insert(4)
 # bst1.insert(7)
-# bst1.insert(10)
-# bst1.insert(14)
+bst1.insert(10)
+bst1.insert(14)
 # bst1.insert(13)
 
-# bst1.depth_first_for_each()
+# print(bst1.value)
+# print(bst1.left.value)
+# print(bst1.left.left.value)
+# print(bst1.right.value)
+# print(bst1.right.right.value)
+
+bst1.depth_first_for_each()
