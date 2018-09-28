eede084e246f9da338a966b3c47544e802e9945d
Adrian Adames <adrianadames@gmail.com>
2018-09-27 22:21:23 -0400

    Breadth first search traversal in progress.

--- data_structures/ex_1/binary_search_tree.py
+++ data_structures/ex_1/binary_search_tree.py
@@ -9,6 +9,19 @@ class Stack:
     def pop(self):
       return self.items.pop()
 
+# Queue's alternative name is FIFO (first in, first out)
+class Queue:
+  def __init__(self):
+    self.items = []
+  
+  #Insert item to end of queue
+  def enqueue(self, item):
+    self.items.append(item)
+
+  #Delete first item in the queue
+  def dequeue(self):
+    return self.items.pop(0)
+
 class BinarySearchTree:
   def __init__(self, value):
     self.value = value
@@ -35,7 +48,29 @@ class BinarySearchTree:
     print(dfsContainer)
 
   def breadth_first_for_each(self, cb):
-    pass
+    bfsContainer = []
+    queue = Queue()
+    currentNode = self
+    finished = False
+    traversed = []
+
+    while finished is False:
+      if currentNode is not None:
+        queue.enqueue(currentNode)
+        # bfsContainer.append(currentNode.value)
+        if currentNode.left:
+          queue.enqueue(currentNode.left)
+        if currentNode.right:
+          queue.enqueue(currentNode.right)
+        traversed.append(currentNode)
+        removed = queue.dequeue()
+        if removed is not in traversed:
+          currentNode = currentNode.
+        else:
+          finished = True
+
+        
+
 
   def insert(self, value):
     new_tree = BinarySearchTree(value)
@@ -82,6 +117,14 @@ class BinarySearchTree:
 # print(stack1.pop())
 # print(stack1.items)
 
+# q1 = Queue()
+# q1.enqueue(4)
+# q1.enqueue(5)
+# q1.enqueue(6)
+# q1.enqueue(7)
+# print(q1.items)
+# q1.dequeue()
+# print(q1.items)
 
 
 bst1 = BinarySearchTree(8)
@@ -101,4 +144,5 @@ bst1.insert(14)
 # print(bst1.right.right.value)
 # print(bst1.value)
 
-bst1.depth_first_for_each(lambda x:x)
+# bst1.depth_first_for_each(lambda x:x)
+bst1.breadth_first_for_each(lambda x:x)
